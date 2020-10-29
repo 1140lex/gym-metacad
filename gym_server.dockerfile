0135b7fe1a29b756fc033c12f1f752dca9154197
Ethycs <ethycs@github.com>
2020-10-28 17:01:20 -0700

    Buildable Docker Container

--- gym_server.dockerfile
+++ gym_server.dockerfile
@@ -1,8 +1,13 @@
 FROM nikolaik/python-nodejs 
 COPY . /app
 WORKDIR /app
-RUN apt install golang 
-RUN pip install numpy gym universe -y 
+RUN \
+ apt update -y && \
+ apt upgrade -y && \
+ apt install golang-go gfortran libopenblas-dev liblapack-dev -y && \
+ pip install cython numpy && \
+ pip install gym universe
+WORKDIR /app/metacad
 RUN npm install
 ENTRYPOINT ["npm", "run", "dev"]
-EXPOSE 3000/TCP
+EXPOSE 3000/TCP 5558/TCP
--- tests/test_zeromq.py
+++ tests/test_zeromq.py
@@ -1,12 +1,20 @@
 import time
+import sys
 import zmq
 
-def worker():
+port = 5558
+
+try:
+    if sys.argv[1].isnumeric():
+        port = sys.argv[1]
+
+
+def test_listener():
     context = zmq.Context()
     socket = context.socket(zmq.PULL)
-    socket.connect("tcp://127.0.0.1:3000")
+    socket.connect("tcp://127.0.0.1:" + port)
     while True:
         message = socket.recv()
         print( "Received:", message)
 
-worker()
+test_listener()
