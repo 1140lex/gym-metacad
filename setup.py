8f85a437ccbd29ee3bbe417bcc91db59d1f35392
Ethycs <ethycs@github.com>
2020-11-18 15:14:17 -0800

    Privacy Cleanup and GPG Test

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(name='gym_metacad',
 	version='0.0.1',
-	author="Jasper Yao",
-	author_email="jasper.yao.iso@gmail.com",
+	author="Jasper",
+	author_email="Ethycs@users.noreply.github.com",
 	description='OpenAI gym integration into MetaCAD',
-	install_requires=['numpy','gym', 'pyppeteer', 'pyzmq', 'python-socketio', 'uvicorn']
+	install_requires=['numpy','gym', 'pyppeteer', 'python-socketio', 'uvicorn']
 )
